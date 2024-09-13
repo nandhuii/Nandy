85380259a44938692ba5332a0fe905f06c88f431
https://github.com/nandhuii/Nandy.git <nandynandy.240799@gmail.com>
2024-09-13 12:44:06 +0530

    hi

---
+++ src/test/java/org/A/Git.java
@@ -0,0 +1,15 @@
+package org.A;
+
+public class Git {
+	private void company() {
+		System.out.println("compname");
+		}
+		private void sample() {
+		System.out.println("sampId");
+		}
+		private void employee() {
+		System.out.println("empId");
+		}
+		}
+
+
